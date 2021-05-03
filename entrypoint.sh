e230eccde20fe8306930660c5b5039b894a61ed9
Josiah England <jengland@redhat.com>
2021-05-03 12:30:36 -0600

    Install galaxy roles for runner service and copy them container user's home dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-if [ `id -u` -gt 1000000 ]; then
-    echo "runner:x:`id -u`:`id -g`:,,,:/runner:/bin/bash" > /tmp/passwd
-    cat /tmp/passwd >> /etc/passwd
-    rm /tmp/passwd
-fi
-
 if [ -e /ansible/requirements.yml ]; then
-    ansible-galaxy install -r /ansible/requirements.yml -p /runner/.ansible/roles
-    cp -r /runner/.ansible/roles /.ansible/roles
+  echo "Installing roles for local ansible user"
+  ansible-galaxy install -i -r /ansible/requirements.yml -p /ansible/roles
 fi
-
+if [ `id -u` -gt 1000000 ]; then
+  echo "runner:x:`id -u`:`id -g`:,,,:/runner:/bin/bash" > /tmp/passwd
+  cat /tmp/passwd >> /etc/passwd
+  rm /tmp/passwd
+  if [ -e /ansible/requirements.yml ]; then
+    echo "Installing roles for runner"
+    cp -rv /ansible/roles /runner/components/
+    cp -rv /ansible/roles /.ansible/
+  fi
+fi
 echo Running: $@
 exec /bin/tini -- $@
